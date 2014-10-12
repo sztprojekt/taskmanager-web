706eefbd11763fdfa4ee9793799aad90de68801f
adamors <adamors@users.noreply.github.com>
2014-10-12 22:52:46 +0300

    WEB-3 setting up database and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141012171538) do
+ActiveRecord::Schema.define(version: 20141012193227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.string   "name",                         null: false
+    t.datetime "completed_at"
+    t.boolean  "status",       default: false
+    t.integer  "user_id",                      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +44,8 @@ ActiveRecord::Schema.define(version: 20141012171538) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role_id"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['registered', 'admin'].each do |role|
+  Role.find_or_create_by({name: role})
+end
+
+unless User.count > 0
+  role_admin = Role.find_by_name 'admin'
+  User.create({ email: 'admin@example.com', password: 'admin12345', role: role_admin})
+end
+
