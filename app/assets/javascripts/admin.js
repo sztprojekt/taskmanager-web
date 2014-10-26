402b0e3e5424793be6570ff9a0053a86ecd09529
adamors <adamors@users.noreply.github.com>
2014-10-26 17:10:56 +0200

    Fixing Angular dependency names for minification.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -6,7 +6,7 @@
 var moduleUrl = '/assets/';
 
 var app = angular.module('admin_panel', ['ngRoute'])
-    .config(function ($routeProvider){
+    .config(['$routeProvider', function ($routeProvider){
         $routeProvider
         .when('/', {
             redirectTo: '/dashboard',
@@ -18,8 +18,8 @@ var app = angular.module('admin_panel', ['ngRoute'])
         .otherwise({
             redirectTo: '/'
         });
-    })
-    .run(function($http, $rootScope, $window) {
+    }])
+    .run(['$http', '$rootScope', '$window', function($http, $rootScope, $window) {
         $http.defaults.headers.common['Accept'] = 'application/json';
         $http.defaults.headers.common['Content-Type'] = 'application/json';
         $rootScope.logout = function(){
@@ -27,10 +27,10 @@ var app = angular.module('admin_panel', ['ngRoute'])
                 $window.location = $window.siteUrl;
             });
         };
-    });
+    }]);
 
-app.controller('DashboardCtrl', function($scope, $http) {
+app.controller('DashboardCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('/user').success(function(data){
         $scope.user = data.data.user;
     });
-});
+}]);
